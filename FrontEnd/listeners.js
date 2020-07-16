0022fee97ed05b47d1009cb832ac4ea138045de1
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-16 13:21:35 +0200

    Added fake http code

--- FrontEnd/listeners.js
+++ FrontEnd/listeners.js
@@ -1,12 +1,20 @@
 import * as elements from "./elements.js";
+import Memo from "./memo.js";
+import Http from "./http.js";
 
-function addMemo() {
-  const newMemoText = elements.noteInput.nodeValue;
+async function addMemo() {
+  const newMemoText = elements.noteInput.value;
   if (newMemoText === null || newMemoText.length == 0) {
     elements.emptyModal.classList.add("is-active");
     return;
   }
   console.log("Memo created: " + newMemoText);
+  const memo = new Memo(newMemoText);
+  try {
+    await Http.storeMemo("link" + newMemoText);
+  } catch {
+    alert("Shit");
+  }
 }
 
 function closeEmptyMemoModal() {
