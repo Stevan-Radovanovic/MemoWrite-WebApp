b61cc6cad916ac5d121f2b5b4952f2e6e7d18398
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-16 13:10:03 +0200

    added storing memos

--- FrontEnd/http.js
+++ FrontEnd/http.js
@@ -13,4 +13,19 @@ export default class Http {
       http.send();
     });
   }
+
+  static storeMemo(url) {
+    return new Promise((resolve, reject) => {
+      const http = new XMLHttpRequest();
+      http.open("POST", url);
+      http.onStateChanged = function () {
+        if (http.readyState === DONE && http.status < 400) {
+          resolve(JSON.parse(http.responseText));
+        } else if (http.readyState === DONE) {
+          reject("There was a problem with the server...");
+        }
+      };
+      http.send();
+    });
+  }
 }
